9614cfb3e83483d7c0f61ab84713b25cd9212144
Anselm Peter <anselm.peter@iav.de>
2023-12-01 12:07:44 +0100

    Modifications
    Changed to ternary operator. Now finding input file when executed from
    VSCode

--- Day_01/advent-01.1.py
+++ Day_01/advent-01.1.py
@@ -1,7 +1,7 @@
 import re
 
 sum = 0
-for line in open('input-advent-01.1.txt'):
+for line in open('./Day_01/input-advent-01.1.txt'):
     all = re.findall('\d', line)
     combine = all[0] + all[-1]
     number = int(combine)
--- Day_01/advent-01.2.py
+++ Day_01/advent-01.2.py
@@ -4,16 +4,13 @@ numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine
 regex = re.compile('(?=(\d|' + '|'.join(numbers) + '))')
 
 def string_to_num(str):
-    if str in numbers:
-        return numbers.index(str) + 1
-    else:
-        return int(str)
+    return (numbers.index(str) + 1 if (str in numbers) else int(str))
 
 def match_to_sum(first, second):
     return string_to_num(first.group(1)) * 10 + string_to_num(second.group(1))
 
 sum = 0
-for line in open('input-advent-01.1.txt'):
+for line in open('./Day_01/input-advent-01.1.txt'):
     it = regex.finditer(line)
     matches = list(it)
     sum += match_to_sum(matches[0], matches[-1])
