a8bb49dfdb07bf5db12f9887e78904f99d86792d
Anselm Peter <anselm.peter@web.de>
2023-12-02 09:20:25 +0100

    Minor refactoring of day 2

--- Day_02/advent-02.1.py
+++ Day_02/advent-02.1.py
@@ -2,8 +2,7 @@ import numpy
 import re
 
 def scan(round, r):
-    m = r.search(round)
-    if m:
+    if m := r.search(round):
         return int(m.group(1))
     else:
         return 0
@@ -11,9 +10,6 @@ def scan(round, r):
 RED, GREEN, BLUE = 12, 13, 14
 reGame = re.compile(r'Game\s(\d+):')
 reRound = [ re.compile(r'(\d+) red'), re.compile(r'(\d+) green'), re.compile(r'(\d+) blue') ]
-reRed = re.compile(r'(\d+) red')
-reGreen = re.compile(r'(\d+) green')
-reBlue = re.compile(r'(\d+) blue')
 
 sumOfGames = 0
 sumOfPowers = 0
@@ -21,15 +17,17 @@ for line in open('./Day_02/input-advent-02.1.txt'):
     # Get game number
     matchGame = reGame.search(line)
     gameNumber = int(matchGame.group(1))
-    rounds = line[matchGame.end():].split(';')
 
+    # Analyze rounds
+    rounds = line[matchGame.end():].split(';')
     roundMax = [0, 0, 0]
     for round in rounds:
         for i in range(3):
             roundMax[i] = max(roundMax[i], scan(round, reRound[i]))
     if roundMax[0] <= RED and roundMax[1] <= GREEN and roundMax[2] <= BLUE:
-        print('Round %d valid' % gameNumber)
         sumOfGames += gameNumber
+
+    # Part 2: Calculate "power" for *all* rounds
     power = numpy.prod(roundMax)
     sumOfPowers += power
 
