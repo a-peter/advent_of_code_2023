87422a7c5852a4e82423bf2e60c3f53a8466392c
Anselm Peter <anselm.peter@iav.de>
2023-12-05 14:44:26 +0100

    Refactored mapping ranges

--- Day_05/advent-05.1.py
+++ Day_05/advent-05.1.py
@@ -2,18 +2,23 @@ import sys
 
 seeds = [] # list of numbers with seeds, part one
 seeds_ranges = [] # list of seeds ranges, part two
-ranges = [] # list of dictionaries
+mapping_ranges = [] # list of lists of tuples with mapping areas
+
+SOURCE_START = 0
+SOURCE_END =   1
+DEST_START =   2
+DEST_END =     3
 
 def add_line_to_map(line):
     values = [int(value) for value in line.split()]
-    return (values[1], values[0], values[2], values[1] + values[2] - 1, values[0] + values[2] - 1)
+    return (values[1], values[1] + values[2] - 1, values[0], values[0] + values[2] - 1)
 
 def add_mapping(mapping):
     mapping.sort(key=lambda x: x[0])
     if mapping[0][0] != 0:
-        mapping = [(0, 0, mapping[0][0], mapping[0][0] - 1, mapping[0][0] - 1)] + mapping
-    mapping.append((mapping[-1][0] + mapping[-1][2], mapping[-1][0] + mapping[-1][2], 10000000000000000000, mapping[-1][0] + 10000000000000000000, mapping[-1][0] + 10000000000000000000 ))
-    ranges.append(mapping)
+        mapping = [(0, mapping[0][0] - 1, 0, mapping[0][0] - 1)] + mapping
+    mapping.append((mapping[-1][0] + mapping[-1][2], mapping[-1][0] + 10000000000000000000, mapping[-1][0] + mapping[-1][2], mapping[-1][0] + 10000000000000000000 ))
+    mapping_ranges.append(mapping)
 
 def read_input(file_name):
     global seeds
@@ -34,7 +39,7 @@ def read_input(file_name):
             mapping_list = []
     add_mapping(mapping_list)
 
-def process_mappings():
+def perform_task_1():
     mappings = []
     min_location = sys.maxsize
     for seed in seeds:
@@ -42,12 +47,11 @@ def process_mappings():
             min_location = min(min_location, mappings[-1])
         mappings = [seed]
         m = seed
-        for index, r in enumerate(ranges):
-            for index2, section in enumerate(r):
-                if m <= section[0] + section[2] - 1:
-                    mappings.append(section[1] + m - section[0])
+        for mapping in mapping_ranges:
+            for mapping_range in mapping:
+                if m <= mapping_range[SOURCE_END]:
+                    mappings.append(mapping_range[DEST_START] + m - mapping_range[SOURCE_START])
                     m = mappings[-1]
-                    # print(seed, index, r)
                     break
     min_location = min(min_location, mappings[-1])
     return min_location
@@ -58,30 +62,33 @@ def find_minimum(current_min, mappings):
         x = min(current_min, min(mappings[7], key = lambda m: m[0])[0])
     return x
 
-def process_more_seeds():
-    mappings = {}
+def perform_task_2():
+    stage_ranges = {}
     min_location = sys.maxsize
     for seed_range in seeds_ranges:
-        min_location = find_minimum(min_location, mappings)
-        mappings[0] = [(seed_range[0], seed_range[0] + seed_range[1] - 1)]
-        for index, r in enumerate(ranges):
-            mappings[index + 1] = []
-            for m in mappings[index]:
-                for section in r:
-                    if m[0] <= section[3]: # start of seed-range in section?
-                        if m[1] <= section[3]: # end of seed-range in section?
+        min_location = find_minimum(min_location, stage_ranges)
+        stage_ranges[0] = [(seed_range[0], seed_range[0] + seed_range[1] - 1)]
+        # iterate all mappings
+        for index, mapping in enumerate(mapping_ranges):
+            stage_ranges[index + 1] = []
+            for stage_range in stage_ranges[index]:
+                # iterate all lines of a mapping
+                for section in mapping:
+                    if stage_range[0] <= section[SOURCE_END]: # start of seed-range in section?
+                        if stage_range[1] <= section[SOURCE_END]: # end of seed-range in section?
                             # fits into, add whole mapping rage
-                            mappings[index + 1].append((m[0] - section[0] + section[1], m[1] - section[0] + section[1]))
+                            stage_ranges[index + 1].append((stage_range[0] - section[SOURCE_START] + section[DEST_START], stage_range[1] - section[SOURCE_START] + section[DEST_START]))
                             break
                         else:
-                            # NOT OK!!!
-                            partial_mapping = (m[0] - section[0] + section[1], section[4])
-                            mappings[index + 1].append(partial_mapping)
-                            next_mapping = (section[3] + 1, m[1])
-                            m = next_mapping
+                            # stage range does not fit into current mapping line
+                            # split up ...
+                            partial_mapping = (stage_range[0] - section[SOURCE_START] + section[DEST_START], section[DEST_END])
+                            stage_ranges[index + 1].append(partial_mapping)
+                            # and continue with partial range and next section in mapping
+                            stage_range = (section[SOURCE_END] + 1, stage_range[1])
                             continue
 
-    min_location = find_minimum(min_location, mappings)
+    min_location = find_minimum(min_location, stage_ranges)
     return min_location
 
 print('input:')
@@ -90,6 +97,6 @@ read_input('Day_05/input-advent-05.1.txt')
 
 print()
 print('mappings')
-print('minimum location:', process_mappings())
-print('minimum location 2:', process_more_seeds())
+print('minimum location  :', perform_task_1())
+print('minimum location 2:', perform_task_2())
 
