86524ec2e76f3ba0fea3b9c3029566d075503a28
Anselm Peter <anselm.peter@iav.de>
2023-12-06 08:59:33 +0100

    Optimized Day 06

--- Day_06/advent-06.1.py
+++ Day_06/advent-06.1.py
@@ -1,6 +1,21 @@
 from functools import reduce
 import math
 
+def calc_solution(time, distance):
+    solution1 = (time/2) + math.sqrt((time * time)/4 - distance)
+    solution2 = (time/2) - math.sqrt((time * time)/4 - distance)
+
+    # Special case: the zeroes are without decimals
+    if math.floor(solution1) == solution1:
+        solution1 -= 1
+    if math.ceil(solution2) == solution2:
+        solution2 += 1
+
+    result = math.floor(solution1) - math.ceil(solution2) + 1
+    return result
+
+#####################
+# Read input
 input = []
 input2 = []
 
@@ -9,37 +24,18 @@ for line in open('./Day_06/input-advent-06.1.txt'):
     input.append([int(i) for i in line[line.index(':') + 1:].split()])
     input2.append(int(line[line.index(':') + 1:-1].replace(' ', '')))
 
-number_of_choices = []
-for race in range(len(input[0])):
-    choices_for_race = 0
-    race_time = input[0][race]
-    for down_time in range(race_time + 1):
-        choices_for_race += int(down_time * (race_time - down_time) > input[1][race])
-    number_of_choices.append(choices_for_race)
+#####################
+# Task 2
 
-task_1 = reduce(lambda x,y: x * y, number_of_choices)
+task_1 = 1
+for race in range(len(input[0])):
+    task_1 *= calc_solution(input[0][race], input[1][race])
+
 print('Task 1: %d mm' % task_1)
 # should be 219849
 
-#faster for task2:
-time = input2[0]
-distance = input2[1]
+#####################
+# Task 2
 
-solution1 = (time/2) + math.sqrt((time * time)/4 - distance)
-solution2 = (time/2) - math.sqrt((time * time)/4 - distance)
-result = math.floor(solution1) - math.ceil(solution2) + 1
-
-# print(math.floor(solution1))
-# print(math.ceil(solution2))
-print('Task 2: %d mm' % result)
-# time: 38 ms :-D
-# pass
-
-# race_time = input2[0]
-# choices_for_race = 0
-# for down_time in range(race_time + 1):
-#     # my_distance = down_time * (race_time - down_time)
-#     choices_for_race += int(down_time * (race_time - down_time) > input2[1])
-# print('Task 2: %d' % choices_for_race)
+print('Task 2: %d mm' % calc_solution(input2[0], input2[1]))
 # should be 29432455
-# time: 11 seconds
