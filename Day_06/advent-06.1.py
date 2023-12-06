802cc90dd552fb17a5fa00d533f284424237d52f
Anselm Peter <anselm.peter@iav.de>
2023-12-06 08:39:34 +0100

    Only fast method for Day 06, Task 2

--- Day_06/advent-06.1.py
+++ Day_06/advent-06.1.py
@@ -18,7 +18,7 @@ for race in range(len(input[0])):
     number_of_choices.append(choices_for_race)
 
 task_1 = reduce(lambda x,y: x * y, number_of_choices)
-print('Task 1: %d' % task_1)
+print('Task 1: %d mm' % task_1)
 # should be 219849
 
 #faster for task2:
@@ -27,17 +27,19 @@ distance = input2[1]
 
 solution1 = (time/2) + math.sqrt((time * time)/4 - distance)
 solution2 = (time/2) - math.sqrt((time * time)/4 - distance)
+result = math.floor(solution1) - math.ceil(solution2) + 1
 
-print(math.floor(solution1))
-print(math.ceil(solution2))
-print(math.floor(solution1) - math.ceil(solution2) + 1)
-pass
+# print(math.floor(solution1))
+# print(math.ceil(solution2))
+print('Task 2: %d mm' % result)
+# time: 38 ms :-D
+# pass
 
-race_time = input2[0]
-choices_for_race = 0
-for down_time in range(race_time + 1):
-    # my_distance = down_time * (race_time - down_time)
-    choices_for_race += int(down_time * (race_time - down_time) > input2[1])
-print('Task 2: %d' % choices_for_race)
+# race_time = input2[0]
+# choices_for_race = 0
+# for down_time in range(race_time + 1):
+#     # my_distance = down_time * (race_time - down_time)
+#     choices_for_race += int(down_time * (race_time - down_time) > input2[1])
+# print('Task 2: %d' % choices_for_race)
 # should be 29432455
 # time: 11 seconds
