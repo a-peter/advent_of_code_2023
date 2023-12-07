07dcebe323820def3458d11ae68abeb639fe520d
Anselm Peter <anselm.peter@iav.de>
2023-12-07 09:14:17 +0100

    Added Task 2 of Day 7

--- Day_07/advent-07.py
+++ Day_07/advent-07.py
@@ -1,15 +1,25 @@
 import functools
-import math
+
+file_name = './Day_07/input-advent-07.txt'
+# file_name = './Day_07/sample-07.txt'
 
 kinds = list('AKQJT98765432')
 kinds_2 = list('AKQT98765432J')
-# FIVE, FOUR, FULL, THREE, TWO, ONE, HIGH = 6, 5, 4, 3, 2, 1, 0
-def get_type(cards: str):
+
+def get_type(cards: str, jokers = False):
     nums = []
+    if jokers:
+        number_of_jokers = cards.count('J')
+        cards = cards.replace('J', '')
     while len(cards) > 0:
         nums.append(cards.count(cards[0]))
         cards = cards.replace(cards[0], '')
     nums.sort(reverse=True)
+    if jokers:
+        if len(nums) == 0:
+            nums.append(number_of_jokers)
+        else:
+            nums[0] += number_of_jokers
     return nums[0] * (5 - len(nums))
 
 def sorter_cmp(a, b):
@@ -22,10 +32,18 @@ def sorter_cmp(a, b):
                 return delta
         return 0
 
+def sorter_cmp_with_jokers(a, b):
+    if b[2] != a[2]:
+        return a[2] - b[2]
+    else:
+        for i in range(5):
+            delta = kinds_2.index(b[0][i]) - kinds_2.index(a[0][i])
+            if delta != 0:
+                return delta
+        return 0
 
 hands = []
-# for line in open('./Day_07/input-advent-07.txt'):
-for line in open('./Day_07/sample-07.txt'):
+for line in open(file_name):
     hand = line.split()
     cards, bid, t = hand[0], int(hand[1]), get_type(hand[0])
     hands.append([cards, bid, t])
@@ -42,3 +60,22 @@ for index, hand in enumerate(hands):
     sum += hand[1] * (index + 1)
 
 print('Task 1: Winning %d' % sum)
+
+#########################
+
+hands = []
+for line in open(file_name):
+    hand = line.split()
+    cards, bid, t = hand[0], int(hand[1]), get_type(hand[0], True)
+    hands.append([cards, bid, t])
+    print(hands[-1])
+hands.sort(key=functools.cmp_to_key(sorter_cmp_with_jokers))
+
+print()
+[print(hand) for hand in hands]
+
+sum = 0
+for index, hand in enumerate(hands):
+    sum += hand[1] * (index + 1)
+
+print('Task 2: Winning %d' % sum)
