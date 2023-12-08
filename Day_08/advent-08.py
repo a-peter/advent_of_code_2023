0526b9e5003ea02c5e0b233205a14ccbcd5d342a
Anselm Peter <anselm.peter@iav.de>
2023-12-08 10:54:12 +0100

    Optimized Day 8, Task 1 and 2

--- Day_08/advent-08.py
+++ Day_08/advent-08.py
@@ -7,73 +7,16 @@ from math import lcm
 # file_name = './Day_08/sample-08.3.txt'
 file_name = './Day_08/input-advent-08.txt'
 
-# def next_instruction(instruction_index):
-#     instruction = int(instructions[instruction_index] == 'R')
-#     instruction_index = (instruction_index + 1) % len(instructions)
-#     return (instruction, instruction_index)
+instructions, _, *str_navigation = open(file_name).read().splitlines()
+navigation = {nav[0:3]: (nav[7:10], nav[12:15]) for nav in str_navigation}
+starting_nodes = [nav for nav in navigation if nav[-1] == 'A']
 
-regex = re.compile('(\w+) = \((\w+), (\w+)\)')
-navigation = dict()
-starting_nodes = []
-for i, line in enumerate(open(file_name)):
-    if i == 0:
-        instructions = line.strip()
-    elif i >= 2:
-        match = regex.search(line)
-        navigation[match.group(1)] = (match.group(2), match.group(3))
-        if match.group(1).endswith('A'):
-            starting_nodes.append(match.group(1))
-
-
-# Task 1
-# location, end = 'AAA', 'ZZZ'
-# instruction_index = 0
-# steps = 0
-# while True:
-#     instruction, instruction_index = next_instruction(instruction_index)
-#     location, steps = (navigation[location][instruction], steps + 1)
-#     if location == end:
-#         break
-# print('Task 1: %d steps' % steps)
-# should be 19951 for input-advent-08.txt
-
-# Task 2
-instruction_index = 0
-steps = 0
-
-instruction_cylcer = cycle(int(i == 'R') for i in instructions)
-iterations = []
+iterations = [0] * len(starting_nodes)
 for i, pos in enumerate(starting_nodes):
-    steps = 0
     instruction_cylcer = cycle(int(i == 'R') for i in instructions)
-    while not pos[-1] == 'Z':
-        steps += 1
+    while pos[-1] == 'Z':
+        iterations[i] += 1
         pos = navigation[pos][next(instruction_cylcer)]
-    iterations.append(steps)
 
-print('Task 1: %d steps' % iterations[starting_nodes.index('AAA')])
-print('Task 2: %d steps' % lcm(*iterations))
-
-last_step = 0
-while True:
-    instruction = next(instruction_cylcer)
-
-    # all_finished = True
-    # for i, node in enumerate(starting_nodes):
-    #     starting_nodes[i] = navigation[starting_nodes[i]][instruction]
-    #     all_finished = all_finished and starting_nodes[i][-1] == 'Z'
-
-    all_finished = False #True
-    i = 6
-    starting_nodes[i] = navigation[starting_nodes[i]][instruction]
-    if starting_nodes[i][-1] == 'Z':
-        print(steps, steps - last_step)
-        last_step = steps
-        # starting_nodes[i] = navigation[node][instruction]
-        # all_finished = all_finished and starting_nodes[i][-1] == 'Z'
-
-    steps += 1
-    if all_finished:
-        break
-
-print('Task 1: %d steps' % steps)
+print('Task 1: %d steps' % iterations[starting_nodes.index('AAA')]) # should be 19951
+print('Task 2: %d steps' % lcm(*iterations)) # should be 16342438708751
