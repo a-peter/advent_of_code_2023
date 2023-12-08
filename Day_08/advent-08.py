1448cdccef32f02fa7e4473d5467363f1daa08ac
Anselm Peter <anselm.peter@iav.de>
2023-12-08 11:04:25 +0100

    Added remark on [-1] and .endswith()

--- Day_08/advent-08.py
+++ Day_08/advent-08.py
@@ -1,20 +1,24 @@
-import re
 from itertools import cycle
 from math import lcm
+
+# REMARK: string[-1] == 'x' is faster than string.endswith('x')
 
 # file_name = './Day_08/sample-08.txt'
 # file_name = './Day_08/sample-08.2.txt'
 # file_name = './Day_08/sample-08.3.txt'
+# file_name = './Day_08/sample-08.4.txt'
 file_name = './Day_08/input-advent-08.txt'
 
 instructions, _, *str_navigation = open(file_name).read().splitlines()
 navigation = {nav[0:3]: (nav[7:10], nav[12:15]) for nav in str_navigation}
 starting_nodes = [nav for nav in navigation if nav[-1] == 'A']
+# starting_nodes = [nav for nav in navigation if nav.endswith('A')]
 
 iterations = [0] * len(starting_nodes)
 for i, pos in enumerate(starting_nodes):
     instruction_cylcer = cycle(int(i == 'R') for i in instructions)
-    while pos[-1] == 'Z':
+    while pos[-1] != 'Z':
+    # while not pos[-1].endswith('Z'):
         iterations[i] += 1
         pos = navigation[pos][next(instruction_cylcer)]
 
