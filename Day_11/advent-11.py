114d52533bc8f1029e14180338f0a548967d41cf
Anselm Peter <anselm.peter@iav.de>
2023-12-11 08:07:18 +0100

    Day 11, Task 2

--- Day_11/advent-11.py
+++ Day_11/advent-11.py
@@ -1,7 +1,6 @@
 
-
-file_name = "./Day_11/sample-11.1.txt"
-file_name = "./Day_11/input-advent-11.txt"
+file_name, factor = "./Day_11/sample-11.1.txt", 99
+file_name, factor = "./Day_11/input-advent-11.txt", 999999
 
 universe = [[x for x in line] for line in open(file_name).read().splitlines()]
 
@@ -15,16 +14,17 @@ for y,line in enumerate(universe):
             universe[y][x] = universe_number
             universe_number += 1
 
-# expand
-for y in reversed(range(len(universe))):
-    if sum(universe[y]) == 0:
-        universe.insert(y, [0] * len(universe[0]))
+# expand by storing added rows and columns
+rows = [row_number for row_number,row in enumerate(universe) if sum(row) == 0]
 
+cols = []
 for x in reversed(range(len(universe[0]))):
     column = [universe[y][x] for y in range(len(universe)) ]
     if sum(column) == 0:
-        for y in range(len(universe)):
-            universe[y].insert(x, 0)
+        cols.append(x)
+
+rows.sort()
+cols.sort()
 
 # find galaxies
 galaxies = []
@@ -33,10 +33,18 @@ for x in range(len(universe[0])):
         if universe[y][x] != 0:
             galaxies.append((x, y))
       
-distance = 0          
+distance = 0
+distance2 = 0       
 for i in range(len(galaxies) - 1):
     for j in range(i+1, len(galaxies)):
-        distance += abs(galaxies[i][0] - galaxies[j][0]) + abs(galaxies[i][1] - galaxies[j][1])
+        xs = [galaxies[i][0], galaxies[j][0]]
+        ys = [galaxies[i][1], galaxies[j][1]]
+        xs.sort()
+        ys.sort()
+        nx = len(list(filter(lambda r: r > xs[0] and r < xs[1], cols)))
+        ny = len(list(filter(lambda c: c > ys[0] and c < ys[1], rows)))
+        distance += abs(galaxies[i][0] - galaxies[j][0]) + abs(galaxies[i][1] - galaxies[j][1]) + nx + ny
+        distance2 += abs(galaxies[i][0] - galaxies[j][0]) + abs(galaxies[i][1] - galaxies[j][1]) + nx * factor + ny * factor
 
-print('Task 1: Sum of distances = %d' % distance)
-pass
+print('Task 1: Sum of distances = %d' % distance) # 374 for sample, 9623138 for input
+print('Task 2: Sum of distances = %d' % distance2) # 1030/8410 for sample, 726820169514 for input
