afd8b4f49cf8722c8845cc3f42769bdb70e30d9e
Anselm Peter <anselm.peter@iav.de>
2023-12-15 10:06:24 +0100

    Refactored Day 15

--- Day_15/advent-15.py
+++ Day_15/advent-15.py
@@ -1,5 +1,8 @@
+from collections import defaultdict
 import functools
 import re
+
+# Optimized, thanks to https://github.com/SteffenHaeussler/advent_of_code/blob/main/day_15/day_15.ipynb
 
 file_name = './Day_15/input-advent-15.txt'
 
@@ -15,45 +18,29 @@ def calc_hash(byte_input: bytes) -> int:
         hash = ((hash + c) * 17 ) % 256
     return hash
 
-sum = 0
-for sequence in INPUT.split(','):
-    input_bytes = sequence.encode('utf-8')
-    sum += calc_hash(input_bytes)
+###########################
+# Task 1
 
+sum = sum([calc_hash(sequence.encode('utf-8')) for sequence in INPUT.split(',')])
 print('Task 1: %d' % sum) # 52 for HASH / 1320 for sample / 512797 for input
 
 ###########################
 # Task 2
 
-def find(label: str, lenses: []) -> int:
-    for i,(lens,_) in enumerate(lenses):
-        if lens == label:
-            return i
-    return -1
-
-map = {i:[] for i in range(256)}
+boxes = defaultdict(dict)
 for sequence in INPUT.split(','):
-    label, focus = re.split(r'[-=]', sequence)
+    label, focal_length = re.split(r'[-=]', sequence)
     box_number = calc_hash(label.encode('utf-8'))
-    # print(label, box_number)
-    lens_index = find(label, map[box_number])
-    if len(focus) > 0: # xxx=7
-        if lens_index == -1:
-            map[box_number].append( (label, int(focus)) )
-        else:
-            map[box_number][lens_index] = (label, int(focus))
-            pass
-    else: # xxx-
-        if lens_index != -1: 
-            lenses = map[box_number]
-            map[box_number] = lenses[:lens_index] + lenses[lens_index + 1:]
-    pass
 
-# Focusing power
+    if focal_length: 
+        boxes[box_number][label] = int(focal_length)
+    else:
+        if label in boxes[box_number]:
+            del boxes[box_number][label]
+
 sum = 0
-for slot in map.keys():
-    for index, (_, focus) in enumerate(map[slot]):
-        sum += (slot + 1) * (index + 1) * focus
-        pass
+for slot,lenses in boxes.items():
+    for index, focal_length in enumerate(lenses.values(), 1):
+        sum += (slot + 1) * index * focal_length
 
 print('Task 2: %d' % sum) # - for HASH / 145 for sample / 262454 for input
