0d8980f070d5ccdb47bb244acaf2377ef91b09a8
Anselm Peter <anselm.peter@iav.de>
2023-12-15 10:37:19 +0100

    Day 15: using reduce for hash

--- Day_15/advent-15.py
+++ Day_15/advent-15.py
@@ -6,27 +6,20 @@ import re
 
 file_name = './Day_15/input-advent-15.txt'
 
-INPUT = 'HASH'
 INPUT = 'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'
 INPUT = open(file_name).read().strip()
 
-
 @functools.cache
 def calc_hash(byte_input: bytes) -> int:
-    hash = 0
-    for c in byte_input:
-        hash = ((hash + c) * 17 ) % 256
-    return hash
+    return functools.reduce(lambda hash, data: ((hash + data) * 17) % 256, byte_input, 0)
 
 ###########################
 # Task 1
-
 sum = sum([calc_hash(sequence.encode('utf-8')) for sequence in INPUT.split(',')])
-print('Task 1: %d' % sum) # 52 for HASH / 1320 for sample / 512797 for input
+print('Task 1: %d' % sum) # 1320 for sample / 512797 for input
 
 ###########################
 # Task 2
-
 boxes = defaultdict(dict)
 for sequence in INPUT.split(','):
     label, focal_length = re.split(r'[-=]', sequence)
