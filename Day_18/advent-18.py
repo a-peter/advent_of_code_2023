3df0a6cec21ac1095ae13145331767d477274c4a
Anselm Peter <anselm.peter@iav.de>
2023-12-18 14:53:10 +0100

    Day 18: cleaned up

--- Day_18/advent-18.py
+++ Day_18/advent-18.py
@@ -6,13 +6,11 @@ import scipy.ndimage as ndi
 file_name = './Day_18/sample-18.1.txt'
 file_name = './Day_18/input-advent-18.txt'
 
-dir_to_int = 'RDLU'
-
 dig_plan_1 = []
 dig_plan_2 = []
 for line in open(file_name).read().splitlines():
     direction, count, col = line.split()
-    dig_plan_1.append([dir_to_int.index(direction), int(count)])
+    dig_plan_1.append(['RDLU'.index(direction), int(count)])
     dig_plan_2.append([int(col[-2]), int(col[2:-2], 16)])
 
 # Expected solutions
@@ -23,27 +21,22 @@ for line in open(file_name).read().splitlines():
 # inner = Area - (boundary/2) + 1
 # Shoelace forumla: https://en.wikipedia.org/wiki/Shoelace_formula
 # Area = [shoelace]
-
-def make_points(position: tuple[int, int], direction: int, count: int) -> tuple[int, int]:
-    if direction == 0: next_position = (position[0] + count, position[1])
-    elif direction == 2: next_position = (position[0] - count, position[1])
-    elif direction == 1: next_position = (position[0], position[1] + count)
-    elif direction == 3: next_position = (position[0], position[1] - count)
-    return next_position
+offsets = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+def make_point(position: tuple[int, int], direction: int, count: int) -> tuple[int, int]:
+    return position[0] + offsets[direction][0] * count, position[1] + offsets[direction][1] * count
 
 def shoelace(points):
-    x = [p[0] for p in points]
-    y = [p[1] for p in points]
+    x, y = zip(*points)
     return 0.5 * abs(sum(x[i]*y[i+1] - x[i+1]*y[i] for i in range(len(points)-1)))
 
 def solve(data: list[tuple[int, int]]) -> int:
     points = [(0, 0)]
     for direction, count in data:
-        points.append(make_points(points[-1], direction, count))
-    area = shoelace(points)
-    boundary_points = sum(line[1] for line in data)
-    inner_points = int(area) - boundary_points // 2 + 1
-    solution = inner_points + boundary_points
+        points.append(make_point(points[-1], direction, count))
+    inner_area = shoelace(points) # The inner area of the "vectors"
+    boundary_points = sum(line[1] for line in data) # The points on the border of the "vectors"
+    inner_points = int(inner_area) - boundary_points // 2 + 1 # see Pick's theorem
+    solution = inner_points + boundary_points 
     return solution
 
 print('Task 1: %d' % solve(dig_plan_1))
