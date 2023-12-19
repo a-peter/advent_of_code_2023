bfa2bea8be756ad3a4a741b94cfd8fa8aa2734b3
Anselm Peter <anselm.peter@iav.de>
2023-12-19 13:06:55 +0100

    Day_19, Task 2: not correct

--- Day_19/advent-19.py
+++ Day_19/advent-19.py
@@ -1,7 +1,8 @@
 import re
+import functools
 
 file_name = './Day_19/sample-19.1.txt'
-file_name = './Day_19/input-advent-19.txt'
+# file_name = './Day_19/input-advent-19.txt'
 
 workflow_text, parts_text = open(file_name).read().split('\n\n')
 
@@ -43,3 +44,45 @@ for part in parts:
         pass
 
 print('Task 1: %d' % ratings) # 19114 / 323625
+
+##############################
+# Task 2
+
+sum_of_pathes = 0
+def check_ranges(ranges: dict[str, list[int]], wf, path: str):
+    global sum_of_pathes
+    leftover = {k:[x for x in v] for (k,v) in ranges.items()}
+    for cat,op,val,target in wf[0]:
+        ranges_copy = {k:[x for x in v] for (k,v) in leftover.items()}
+        if target == 'R':
+            continue
+        if op == '<':
+            ranges_copy[cat][1] = val - 1
+            leftover[cat][0] = val
+        else:
+            ranges_copy[cat][0] = val + 1
+            leftover[cat][1] = val
+        if ranges_copy[cat][0] > ranges_copy[cat][1]:
+            continue
+        if target == 'A':
+            product = functools.reduce(lambda x,y: x*y, [x[1]-x[0]+1 for x in ranges_copy.values()], 1)
+            sum_of_pathes += product
+            print(path, product, ranges_copy)
+        else:
+            check_ranges(ranges_copy, workflow[target], path + ' -> ' + target)
+    if wf[1] == 'A':
+        product = functools.reduce(lambda x,y: x*y, [x[1]-x[0]+1 for x in leftover.values()], 1)
+        sum_of_pathes += product
+        print(path, product, leftover)
+    elif wf[1] == 'R':
+        pass
+    else:
+        check_ranges(leftover, workflow[wf[1]], path + '* ->' + wf[1])
+
+    pass
+
+ranges = {'x': [1, 4000], 'm': [1, 4000], 'a': [1, 4000], 's': [1, 4000]}
+
+wf = workflow['in']
+check_ranges(ranges, wf, 'in')
+print('Task 2: %d' % sum_of_pathes)
