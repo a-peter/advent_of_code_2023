e865f3e94953ad98c40f6ac5d9746361599672c6
Anselm Peter <anselm.peter@web.de>
2023-12-19 16:41:01 +0100

    Day 19: refactored 1

--- Day_19/advent-19.py
+++ Day_19/advent-19.py
@@ -1,9 +1,10 @@
 import re
 import functools
 
+ops = {'<': lambda x,y: x < y, '>': lambda x,y: x > y}
+
 file_name = './Day_19/sample-19.1.txt'
 file_name = './Day_19/input-advent-19.txt'
-
 workflow_text, parts_text = open(file_name).read().split('\n\n')
 
 parts = []
@@ -19,16 +20,9 @@ for line in workflow_text.splitlines():
     rules = [(cat,op,int(val),dest) for cat,op,val,dest  in [rule_reg.match(rule).groups() for rule in match.group(2).split(',') if ':' in rule]]
     workflow[match.group(1)] = (rules, match.group(2).split(',')[-1])
 
-# print(workflow)
-# print(parts)
-
-lt = lambda x,y: x < y
-gt = lambda x,y: x > y
-ops = {'<': lt, '>': gt}
 
 def process(part: dict[str: tuple[list[tuple[str,str,int,str]], str]], wf) -> bool:
     for cat,op,val,target in wf[0]:
-        # print(cat,op,val,target)
         if ops[op](part[cat], val):
             if target == 'A': return True
             elif target == 'R': return False
@@ -37,19 +31,14 @@ def process(part: dict[str: tuple[list[tuple[str,str,int,str]], str]], wf) -> bo
     elif wf[1] == 'R': return False
     else: return process(part, workflow[wf[1]])
 
-ratings = 0
-for part in parts:
-    if process(part, workflow['in']):
-        ratings += sum(part.values())
-        pass
-
+ratings = sum([sum(part.values()) for part in parts if process(part, workflow['in'])])
 print('Task 1: %d' % ratings) # 19114 / 323625
 
 ##############################
 # Task 2
 
 sum_of_pathes = 0
-def check_ranges(ranges: dict[str, list[int]], wf, path: str):
+def check_ranges(ranges: dict[str, list[int]], wf):
     global sum_of_pathes
     leftover = {k:[x for x in v] for (k,v) in ranges.items()}
     for cat,op,val,target in wf[0]:
@@ -65,24 +54,19 @@ def check_ranges(ranges: dict[str, list[int]], wf, path: str):
         if target == 'A':
             product = functools.reduce(lambda x,y: x*y, [x[1]-x[0]+1 for x in ranges_copy.values()], 1)
             sum_of_pathes += product
-            print(path, product, ranges_copy)
         elif target == 'R':
             pass
         else:
-            check_ranges(ranges_copy, workflow[target], path + ' -> ' + target)
+            check_ranges(ranges_copy, workflow[target])
     if wf[1] == 'A':
         product = functools.reduce(lambda x,y: x*y, [x[1]-x[0]+1 for x in leftover.values()], 1)
         sum_of_pathes += product
-        print(path, product, leftover)
     elif wf[1] == 'R':
         pass
     else:
-        check_ranges(leftover, workflow[wf[1]], path + '* ->' + wf[1])
-
-    pass
+        check_ranges(leftover, workflow[wf[1]])
 
 ranges = {'x': [1, 4000], 'm': [1, 4000], 'a': [1, 4000], 's': [1, 4000]}
 
-wf = workflow['in']
-check_ranges(ranges, wf, 'in')
-print('Task 2: %d' % sum_of_pathes)
+check_ranges(ranges, workflow['in'])
+print('Task 2: %d' % sum_of_pathes) # 167409079868000 / 127447746739409
